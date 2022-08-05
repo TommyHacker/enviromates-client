cb64a493b0628888cd5863a1c8ac0c96189abe6c
abscoding <abigailshadare@yahoo.co.uk>
2022-08-05 17:32:42 +0100

    app.js changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import React from 'react';
 import '../public/styles/css/main.css';
+import Button from '@mui/material/Button';
+
+const name = '';
+
 
 const App = () => {
 	return (
 		<>
-			<h1>boilerplate application</h1>
+			<div className="bodycss">
+				<header> 
+					<div className="top"></div>
+					<br />
+					<br />
+					<h1 className="logo">EnviroMate Logo</h1>
+					<br />
+					<br />
+				</header>
+
+				<body>
+             <h2 className="heading">Welcome, {name}!</h2>
+			 <br />
+			 <br />
+			 <Button variant="contained" style={{
+                  flex: "34%",
+                  minWidth: "200px",
+                  fontSize: "1em",
+                  fontWeight: "bold",
+				  padding: "15px",
+				  margin: "10px"
+                }}>
+				 Create Event
+			 </Button>
+
+			 <Button variant="contained" style={{
+                  flex: "34%",
+                  minWidth: "200px",
+                  fontSize: "1em",
+                  fontWeight: "bold",
+				  padding: "15px",
+				  
+                }}>
+				 Attend Event
+			 </Button>
+
+				</body>
+			
+			<footer className="footer">
+
+
+			</footer>
+			
+			</div>
 		</>
 	);
 };
