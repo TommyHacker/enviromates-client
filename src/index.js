23a3766aee4fe8bb4e1c7a1e01ec4a4f9dc6aafd
Stu <stuart.judd.bsc@gmail.com>
2022-08-05 12:46:11 +0100

    redux toolkit addition / App provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./redux-toolkit/store/store";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+
+// Material UI Universal Theme - PLACEHOLDER - CAN BE MOVED INTO OWN MODULE
+const theme = createTheme({
+    typography: {
+      fontFamily: ["Dosis", "BlinkMacSystemFont", '"Segoe UI"', "Roboto"].join(
+        ","
+      ),
+    },
+    palette: {
+      primary: {
+        light: "#665A9F",
+        main: "#8D5FB3",
+      },
+      secondary: {
+        main: "#CCC",
+      },
+    },
+  });
+
+root.render(
+    <ThemeProvider theme={theme}>
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    </ThemeProvider>
+);
