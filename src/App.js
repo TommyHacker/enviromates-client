0039e461fd556f708bc2a53286d2216bfc8dc586
tom harcker <thackerstudent@gmail.com>
2022-08-05 09:44:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../public/styles/css/main.css';
+
+const App = () => {
+	return (
+		<>
+			<h1>boilerplate application</h1>
+		</>
+	);
+};
+
+export default App;
---
+++ src/components/Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Component = () => {
+	return (
+		<>
+			<h2>this is a component component!</h2>
+		</>
+	);
+};
+
+export default Component;
