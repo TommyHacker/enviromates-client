05f8eea8e1359613a145a4f7bb2fb6b61fd0903c
tom harcker <thackerstudent@gmail.com>
2022-08-09 11:54:10 +0100

    started implementing login through api auth

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,45 +1,85 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 
 export default function LoginForm() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [submitDetails, setSubmitDetails] = useState('')
-    const navigate = useNavigate()
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [submitDetails, setSubmitDetails] = useState('');
+	const navigate = useNavigate();
 
-    function handleInput(e){
-        setUsername(e.target.value)
-    }
+	function handleUsername(e) {
+		setUsername(e.target.value);
+	}
 
-    function handlePassword(e){
-        setPassword(e.target.value)
-    }
+	function handlePassword(e) {
+		setPassword(e.target.value);
+	}
 
-    function handleSubmit(e){
-        e.preventDefault()
-        setSubmitDetails(username, password)
-        navigate('/:username', {state: {username: username, password: password}})
-        setUsername('')
-        setPassword('')
-    }
+	function handleSubmit(e) {
+		e.preventDefault();
+		setSubmitDetails(username, password);
+		navigate('/:username', {
+			state: { username: username, password: password },
+		});
+		setUsername('');
+		setPassword('');
+	}
 
-  return (
-    <>
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Type your username" value={username} onChange={handleInput}/>
-            </Form.Group>
+	// POST username and password to the api/users/login
+	const loginHandler = async (e) => {
+		// set payload
+		data = { username, password };
+		try {
+			const response = await axios.post(
+				`${API_URL}/users/`,
+				{ data },
+				{ withcredentials: true }
+			);
+			const res = await response.json();
+			console.log('response from api/users/login.post', res);
+			// username/password AUTH has succeeded, store the access token for future AUTH
+			if (res.data.success === 'True') {
+				window.localStorage.setItem('accesstoken', res.data.accesstoken);
 
-            <Form.Group className="mb-3" controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Type your password" value={password} onChange={handlePassword} />
-            </Form.Group>
-          
-            <Button variant="primary" type="submit">Submit</Button>
-        </Form>
-    </>
-  )
+				// username/password AUTH has failed. redirect to /login again.
+			} else {
+				navigate('/login');
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	return (
+		<>
+			<Form onSubmit={handleSubmit}>
+				<Form.Group className='mb-3' controlId='username'>
+					<Form.Label>Username</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder='Type your username'
+						value={username}
+						onChange={handleUsername}
+					/>
+				</Form.Group>
+
+				<Form.Group className='mb-3' controlId='password'>
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type='password'
+						placeholder='Type your password'
+						value={password}
+						onChange={handlePassword}
+					/>
+				</Form.Group>
+
+				<Button variant='primary' type='submit'>
+					Submit
+				</Button>
+			</Form>
+		</>
+	);
 }
