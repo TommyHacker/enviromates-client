77250dd7f3a5b2a6cb13edaa4b96c09dedfee02d
mariesophiecd <mariesophie.cduran@gmail.com>
2022-08-09 14:03:49 +0100

    added lottie files and created SubmitButton component

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,36 +1,36 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './style.css'
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+// import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
  
-const Map = () => {
+// const Map = () => {
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZ29uZG9sZiIsImEiOiJjbDZrbDg1bHEwMnQ4M2xydmdhZ2Vod3FsIn0.-yMbrRrEw8cJTgrfSyDPhA';
+//     mapboxgl.accessToken = 'pk.eyJ1IjoiZ29uZG9sZiIsImEiOiJjbDZrbDg1bHEwMnQ4M2xydmdhZ2Vod3FsIn0.-yMbrRrEw8cJTgrfSyDPhA';
 
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-0.012);
-    const [lat, setLat] = useState(51.49);
-    const [zoom, setZoom] = useState(11.0);
+//     const mapContainer = useRef(null);
+//     const map = useRef(null);
+//     const [lng, setLng] = useState(-0.012);
+//     const [lat, setLat] = useState(51.49);
+//     const [zoom, setZoom] = useState(11.0);
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/gondolf/cl6kwe4fz003c14mr3doqd0sx',
-            center: [lng, lat],
-            zoom: zoom
-        });
-    });
+//     useEffect(() => {
+//         if (map.current) return; // initialize map only once
+//         map.current = new mapboxgl.Map({
+//             container: mapContainer.current,
+//             style: 'mapbox://styles/gondolf/cl6kwe4fz003c14mr3doqd0sx',
+//             center: [lng, lat],
+//             zoom: zoom
+//         });
+//     });
 
-    useEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-        });
-    });
+//     useEffect(() => {
+//         if (!map.current) return; // wait for map to initialize
+//         map.current.on('move', () => {
+//             setLng(map.current.getCenter().lng.toFixed(4));
+//             setLat(map.current.getCenter().lat.toFixed(4));
+//             setZoom(map.current.getZoom().toFixed(2));
+//         });
+//     });
     
     // useEffect(() => {
     //     map.current.on('load', function(){
@@ -57,12 +57,12 @@ const Map = () => {
     // //     data: ''
     // // })
 
-return (
-    <div>
-        <div className="sidebar"> Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} </div>
-         <div ref={mapContainer} className="map-container" />
-    </div>
-    );
-}
+// return (
+//     <div>
+//         <div className="sidebar"> Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} </div>
+//          <div ref={mapContainer} className="map-container" />
+//     </div>
+//     );
+// }
 
 export default Map
