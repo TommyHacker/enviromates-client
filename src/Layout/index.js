1d7cd50a73b1b7baef6ed3d2a1d24997f194f4a0
Stu <stuart.judd.bsc@gmail.com>
2022-08-10 00:09:14 +0100

    app background

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { NavBar } from '../components'
+import { NavBar, Background } from '../components'
 
 
 export default function Layout() {
 
   return (
     <div>
-        <NavBar />
-        
-        <Outlet />
+      <NavBar />
+         <Background/> 
+      <Outlet />
     </div>
   )
   
