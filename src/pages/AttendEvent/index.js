b5369ed814f60da198d8212167d5cf1c2bdc3af7
mariesophiecd <mariesophie.cduran@gmail.com>
2022-08-10 15:17:23 +0100

    added CSS to Home page

--- src/pages/AttendEvent/index.js
+++ src/pages/AttendEvent/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import { MapStatic } from '../../components'
 
 export default function AttendEvent() {
   return (
     <div>
-    <Button variant="info">Attend Event</Button>{' '}
       <MapStatic/>
     </div>
     
