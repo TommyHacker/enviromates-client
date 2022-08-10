5240daeb4414ca540b3cf7158959945029bb3bb1
tom harcker <thackerstudent@gmail.com>
2022-08-10 11:31:59 +0100

    src to data-src

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import '../public/styles/css/main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
-import * as Pages from './pages';
+import * as Pages from './Pages';
 import Layout from './Layout';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
@@ -49,18 +49,16 @@ const App = () => {
 	});
 
 	return (
-		<>
-			<Routes>
-				<Route path='/' element={<Layout />}>
-					<Route path='/' element={<Pages.Home />} />
-					<Route path='/login' element={<Pages.Login />} />
-					<Route path='/username' element={<Pages.User />} />
-					<Route path='/create-event' element={<Pages.CreateEvent />} />
-					<Route path='/attend-event' element={<Pages.AttendEvent />} />
-					<Route path='/completed-event' element={<Pages.CompletedEvent />} />
-				</Route>
-			</Routes>
-		</>
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route path='/' element={<Pages.Home />} />
+				<Route path='/login' element={<Pages.Login />} />
+				<Route path='/username' element={<Pages.User />} />
+				<Route path='/create-event' element={<Pages.CreateEvent />} />
+				<Route path='/attend-event' element={<Pages.AttendEvent />} />
+				<Route path='/completed-event' element={<Pages.CompletedEvent />} />
+			</Route>
+		</Routes>
 	);
 };
 
--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,71 +1,77 @@
 import React, { useRef, useEffect, useState, useMemo } from 'react';
-import './style.css'
-import { MapContainer, TileLayer, Marker, Popup, useMap, setMap } from 'react-leaflet'
-import  Coordinates  from '../Coordinates'
+import './style.css';
+import {
+	MapContainer,
+	TileLayer,
+	Marker,
+	Popup,
+	useMap,
+	setMap,
+} from 'react-leaflet';
+import Coordinates from '../Coordinates';
 
 function Map() {
-    const [map, setMap] = useState(null)
-    const center = [51.505, -0.09]
-    const zoom = 13
-    const displayMap = useMemo(
-      () => (
-        <div>
-            <MapContainer
-                className='map-container'
-                center={center}
-                zoom={zoom}
-                scrollWheelZoom={false}
-                ref={setMap}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                     <Marker position={[51.505, -0.1075]}>
-                         <Popup>
-                             --ALERT--  <br /> 
-                             Date:  <br/>
-                             Challenge: *** <br />
-                             Host: Chaz <br/>
-                             Attendees: 107 <br/>
-                             <a href="https://example.com">JOIN US</a>
-                         </Popup>
-                     </Marker>
+	const [map, setMap] = useState(null);
+	const center = [51.505, -0.09];
+	const zoom = 13;
+	const displayMap = useMemo(
+		() => (
+			<div>
+				<MapContainer
+					className='map-container'
+					center={center}
+					zoom={zoom}
+					scrollWheelZoom={false}
+					ref={setMap}>
+					<TileLayer
+						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+						url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+					/>
+					<Marker position={[51.505, -0.1075]}>
+						<Popup>
+							--ALERT-- <br />
+							Date: <br />
+							Challenge: *** <br />
+							Host: Chaz <br />
+							Attendees: 107 <br />
+							<a href='https://example.com'>JOIN US</a>
+						</Popup>
+					</Marker>
 
-                     <Marker position={[51.4968, -0.0952]}>
-                         <Popup style={{marginLeft:'55px'}}>
-                             --ALERT--  <br /> 
-                             Date: 17/08 <br/>
-                             Challenge: * <br />
-                             Host: Sarah <br/>
-                             Attendees: 29 <br/>
-                             <a href="https://example.com">JOIN US</a>
-                         </Popup>
-                     </Marker>
+					<Marker position={[51.4968, -0.0952]}>
+						<Popup style={{ marginLeft: '55px' }}>
+							--ALERT-- <br />
+							Date: 17/08 <br />
+							Challenge: * <br />
+							Host: Sarah <br />
+							Attendees: 29 <br />
+							<a href='https://example.com'>JOIN US</a>
+						</Popup>
+					</Marker>
 
-                     <Marker position={[51.499, -0.0864]}>
-                         <Popup>
-                             --ALERT--  <br /> 
-                             Date: 15/08 <br/>
-                             Challenge: **<br />
-                             Host: Simon <br/>
-                             Attendees: 56 <br/>
-                     <a href="https://example.com">JOIN US</a>
-                 </Popup>
-             </Marker>
-            </MapContainer>
-        </div>
-      ),
-      [],
-    )
-  
-    return (
-      <div className='map-box'>
-        {map ? <Coordinates map={map} /> : null}
-        {displayMap}
-      </div>
-    )
-  }
+					<Marker position={[51.499, -0.0864]}>
+						<Popup>
+							--ALERT-- <br />
+							Date: 15/08 <br />
+							Challenge: **
+							<br />
+							Host: Simon <br />
+							Attendees: 56 <br />
+							<a href='https://example.com'>JOIN US</a>
+						</Popup>
+					</Marker>
+				</MapContainer>
+			</div>
+		),
+		[]
+	);
 
-  export default Map
+	return (
+		<div className='map-box'>
+			{map ? <Coordinates map={map} /> : null}
+			{displayMap}
+		</div>
+	);
+}
 
-
+export default Map;
