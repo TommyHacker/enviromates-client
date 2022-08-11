4d5be080b713584a643c8ddc5998c6564904d2b5
abscoding <abigailshadare@yahoo.co.uk>
2022-08-11 13:15:07 +0100

    user page changes

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -10,17 +10,21 @@ export default function User() {
 
 	return (
 		<>
-		<div>
-			<h1 className="usergreet">Hello User!</h1>
+		<div className="contentbg">
+			<h1 className="usergreet">Hello (User)!</h1>
 			<br/>
 			<br/>
-			<br/>
+			<div>
 			<h2 className="usergreet2">Daily Login Streak Day <h2 className="streak">Two</h2></h2>
 			<br/>
 			<img src={trophy} className="streak-img"/>
 			<br/>
 			<br/>
 			<h3 className="usergreet2">You've Earned <h3 className="streak">5 Points!</h3></h3>
+			<br/>
+			<br/>
+			<h3 className="points">Total points earned this month: 136</h3>
+			</div>
 			
 			<Container className="content">
 				{/* <h2>Welcome there, {username}!</h2> */}
