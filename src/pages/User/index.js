50a9620c34c0e50c229a0c99598beffaa940b035
abscoding <abigailshadare@yahoo.co.uk>
2022-08-11 12:07:21 +0100

    user page updates

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,29 +3,45 @@ import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import { Map } from '../../components';
+import trophy from '../../assets/images/trophy.png'
 
 export default function User() {
 	const navigate = useNavigate();
 
 	return (
 		<>
-			<main>
+		<div>
+			<h1 className="usergreet">Hello User!</h1>
+			<br/>
+			<br/>
+			<br/>
+			<h2 className="usergreet2">Daily Login Streak Day <h2 className="streak">Two</h2></h2>
+			<br/>
+			<img src={trophy} className="streak-img"/>
+			<br/>
+			<br/>
+			<h3 className="usergreet2">You've Earned <h3 className="streak">5 Points!</h3></h3>
+			
+			<Container className="content">
 				{/* <h2>Welcome there, {username}!</h2> */}
 				<br />
 				<br />
-				<Button variant='info' onClick={() => navigate('/create-event')}>
+				<Button className="navBtn" variant='info' onClick={() => navigate('/create-event')}>
 					Create an Event
 				</Button>
-				<Button variant='info' onClick={() => navigate('/attend-event')}>
+				<Button className="navBtn" variant='info' onClick={() => navigate('/attend-event')}>
 					Attend an Event
 				</Button>
-			</main>
+				</Container>
 
-			<Container>
+			<br/>
+			<br/>
+
+			{/* <Container>
 				<Map />
-			</Container>
+			</Container> */}
 
-			<footer className='footer'></footer>
+			</div>
 		</>
 	);
 }
