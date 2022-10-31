9e21b3ce67534391482235fb3160f640ceacd602
unknown <thackerstudent@gmail.com>
2022-10-31 21:44:24 +0000

    added switch between list/map view of list all activities

--- src/pages/AllEvents/index.js
+++ src/pages/AllEvents/index.js
@@ -3,11 +3,16 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 import Accordion from 'react-bootstrap/Accordion';
-import './style.css'
+import { Button } from 'react-bootstrap';
+import './style.css';
+import Map from '../../components/Map';
+import { MapStatic } from '../../components';
 
 const AllEvents = () => {
+	// choose between listing events or single map to browse the area
+	const [mapView, setMapView] = useState(false);
 
 	const navigate = useNavigate();
 
@@ -40,37 +45,66 @@ const AllEvents = () => {
 				<h1 className='text-center display-1'>Meet other EnviroMates!</h1>
 				<h4 className='text-center subtitle'>Join one of our events</h4>
 			</Container>
-			<div className='events-list p-4 mx-3 d-flex flex-column justify-content-center'>
-				{events &&
-					events.map((event, index) => {
-						return (
-							<div className='all-events-container'>
-								<motion.div
-									initial={{opacity: 0, }}
-									animate={{opacity: 1}}
-									transition={{ delay: 0.1, duration: 1.2}}
-									exit={{opacity: 0}}
-									>	
-										<Accordion defaultActiveKey="1" className='m-2 p-0 mt-4 '>	
-												<Accordion.Item eventKey="0">
-														<Accordion.Header><h2 className='event-title'>{event.title} <br/><span className='mt-2 challenge-info'> Challenge Level: {event.difficulty} </span></h2>
-														</Accordion.Header>
-															<Accordion.Body className='backgroundColor'>
-																<div className='mb-5'>
-																	<h4><span className='title-main mt-2'>{capitalize(event.title)}</span></h4>
-																		<div  onClick={() => navigateHandler(event.id)} key={index}>
-																			<h5 className='fs-6 mt-3 mb-4'>{capitalize(event.description)}</h5>
-																			<img src={event.img_before} style={{ maxHeight: '200px', maxWidth: '300px', margin:'auto'}} />
-																		</div>
-																</div>
-															</Accordion.Body>
-												</Accordion.Item>
+			<Container className='p-4 d-flex justify-content-center'>
+				<Button className='w-25' onClick={() => setMapView(!mapView)}>
+					{mapView ? 'View List' : 'View Map'}
+				</Button>
+			</Container>
+			{!mapView && (
+				<div className='events-list p-4 mx-3 d-flex flex-column justify-content-center'>
+					{events &&
+						events.map((event, index) => {
+							return (
+								<div className='all-events-container'>
+									<motion.div
+										initial={{ opacity: 0 }}
+										animate={{ opacity: 1 }}
+										transition={{ delay: 0.1, duration: 1.2 }}
+										exit={{ opacity: 0 }}>
+										<Accordion defaultActiveKey='1' className='m-2 p-0 mt-4 '>
+											<Accordion.Item eventKey='0'>
+												<Accordion.Header>
+													<h2 className='event-title'>
+														{event.title} <br />
+														<span className='mt-2 challenge-info'>
+															{' '}
+															Challenge Level: {event.difficulty}{' '}
+														</span>
+													</h2>
+												</Accordion.Header>
+												<Accordion.Body className='backgroundColor'>
+													<div className='mb-5'>
+														<h4>
+															<span className='title-main mt-2'>
+																{capitalize(event.title)}
+															</span>
+														</h4>
+														<div
+															onClick={() => navigateHandler(event.id)}
+															key={index}>
+															<h5 className='fs-6 mt-3 mb-4'>
+																{capitalize(event.description)}
+															</h5>
+															<img
+																src={event.img_before}
+																style={{
+																	maxHeight: '200px',
+																	maxWidth: '300px',
+																	margin: 'auto',
+																}}
+															/>
+														</div>
+													</div>
+												</Accordion.Body>
+											</Accordion.Item>
 										</Accordion>
-								</motion.div>	
-							</div>
-						);
-					})}
-			</div>
+									</motion.div>
+								</div>
+							);
+						})}
+				</div>
+			)}
+			{mapView && <Map events={events} />}
 		</>
 	);
 };
